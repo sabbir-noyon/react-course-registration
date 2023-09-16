86d4e48c660510ae11a370845e8f374d4996c4e3
Sabbir-Noyon <sabbir.noyon.001@gmail.com>
2023-09-16 23:38:30 +0600

    Added Project File and Folders

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="font-bold text-center text-4xl mt-14 text-[#1C1B1B]">Course Registration</h1>
+      <Card></Card>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Card.jsx
@@ -0,0 +1,21 @@
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+
+
+const Card = () => {
+    return (
+        <div>
+           
+            <img src= "../src/assets/images/Rectangle 2.png" ></img>
+            
+            <h3></h3>
+          
+
+            
+        </div>
+    );
+};
+
+export default Card;
